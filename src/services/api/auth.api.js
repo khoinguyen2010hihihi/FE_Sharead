822a49d324b5e05f41746297e05d4277cb32c601
Nguyen Khoi <khoibene@gmail.com>
2025-08-11 23:57:08 +0700

    update auth api

--- src/services/api/auth.api.js
+++ src/services/api/auth.api.js
@@ -1,10 +1,10 @@
 import api from "../../utils/axios.js"
 import { unwrapAll } from "../../utils/unwrapper.js"
 
-export const register = (data) => unwrapAll(api.post('/register', data))
-export const login = (data) => unwrapAll(api.post('/login', data))
-export const logout = () => unwrapAll(api.post('/logout'))
-export const refreshToken = () => unwrapAll(api.post('/refresh-token'))
-export const sendOtp = (data) => unwrapAll(api.post('/send-otp', data))
-export const verifyOtp = (data) => unwrapAll(api.post('/verify-otp', data))
-export const resetPassword = (data) => unwrapAll(api.post('/reset-password', data))
+export const register = (data) => unwrapAll(api.post('/auth/register', data))
+export const login = (data) => unwrapAll(api.post('/auth/login', data))
+export const logout = () => unwrapAll(api.post('/auth/logout'))
+export const refreshToken = () => unwrapAll(api.post('/auth/refresh-token'))
+export const sendOtp = (data) => unwrapAll(api.post('/auth/send-otp', data))
+export const verifyOtp = (data) => unwrapAll(api.post('/auth/verify-otp', data))
+export const resetPassword = (data) => unwrapAll(api.post('/auth/reset-password', data))
